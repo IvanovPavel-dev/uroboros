172ab9aa21491bd58cdf7f65b13a2246f08a89cd
Ivanov Pavel <apostol_fm@mail.ru>
2021-10-19 10:46:12 +0200

    game logic + зшсегкшуы

--- src/components/Battlearea/Battlearea.jsx
+++ src/components/Battlearea/Battlearea.jsx
@@ -1,61 +1,90 @@
 import React, { useState } from "react";
 import s from "./Battlearea.module.css";
-import { v4 as uuidv4 } from "uuid";
+//import { v4 as uuidv4 } from "uuid";
+import pic1 from "./wer.png";
+import pic2 from "./wer1.png";
 
 const Battlearea = () => {
   const [input, setInput] = useState("");
-  const [firstLetter, setFirstLetter] = useState("");
   const [lastLetter, setLastLetter] = useState("");
   const [generalFirstLetter, setGeneralFirstLetter] = useState("");
-  const [lastWord, setLastWord] = useState("");
   const [isFirstPlayer, setIsFirstPlayer] = useState(true);
   const [wordsArray, setWordsArray] = useState([]);
+  const [mooveAnswer, setMooveAnswer] = useState("начнём");
+  let [firstPlayerHarts, setFirstPlayerHearts] = useState(5);
+  let [secondPlayerHarts, setSecondPlayerHearts] = useState(5);
 
-  const onInputChange = (event) => {
-    setInput(event.target.value);
-  };
-  //сделать эту функцию async
-  const onFormSubmit = (event) => {
+  function onFormSubmit(event) {
     event.preventDefault();
-    if (wordsArray.length == 0) {
+
+    if (wordsArray.length === 0) {
       setIsFirstPlayer(false);
+      setWordsArray([...wordsArray, input]);
+      setGeneralFirstLetter(input[0]);
+      setLastLetter(Array.from(input)[input.length - 1]);
+      setMooveAnswer("продолжим");
+    } else {
+      if (wordsArray.includes(input)) {
+        setIsFirstPlayer(isFirstPlayer === true ? false : true);
+        setMooveAnswer("слово уже было!!!");
+        isFirstPlayer === true
+          ? setFirstPlayerHearts(firstPlayerHarts - 1)
+          : setSecondPlayerHearts(secondPlayerHarts - 1);
+      } else {
+        if (lastLetter === input[0]) {
+          setWordsArray([...wordsArray, input]);
+          setLastLetter(Array.from(input)[input.length - 1]);
+          setIsFirstPlayer(isFirstPlayer === true ? false : true);
+          setMooveAnswer("правильно");
+        } else {
+          setIsFirstPlayer(isFirstPlayer === true ? false : true);
+          setMooveAnswer("буквы не совпали!!!");
+          isFirstPlayer === true
+            ? setFirstPlayerHearts(firstPlayerHarts - 1)
+            : setSecondPlayerHearts(secondPlayerHarts - 1);
+        }
+      }
     }
-
-    setWordsArray([...wordsArray, { id: uuidv4(), word: input }]);
-    if (generalFirstLetter === "") {
-      setGeneralFirstLetter(input.split("")[0]);
-    }
-
-    setLastLetter(input.split("").reverse()[0]);
-    console.log(lastLetter === firstLetter ? true : false);
-    console.log(firstLetter);
-    console.log(lastLetter);
-    setFirstLetter(input.split("")[0]);
     setInput("");
-  };
+  }
 
   return (
     <div className={s.battlearea}>
-      <div className={s.player1}>icon1</div>
-      <div className={s.player2}>icon2</div>
-      <div className={s.knopka}>
-        <button onclick="newGameButton()">Start new game BUTTON</button>
+      <div className={s.player1}>
+        <img
+          className={s.img}
+          src="https://i.pinimg.com/originals/54/90/13/549013cde1373f75086b99b2d3c4e423.png"
+          alt={"logo"}
+        />
+        <div>{firstPlayerHarts}</div>
       </div>
+      <div className={s.player2}>
+        <img
+          className={s.img}
+          src="https://i.pinimg.com/originals/fe/2a/7d/fe2a7d2785461fd4d56707853817bef4.png"
+          alt={"logo"}
+        />
+
+        <div>{secondPlayerHarts}</div>
+      </div>
+
       <div className={s.letter}>
+        <div className={s.knopka}>
+          <button onclick="newGameButton()">Start new game BUTTON</button>
+        </div>
         <div>{generalFirstLetter}</div>
-        <form onSubmit={onFormSubmit}>
-          <input
-            type="text"
-            placeholder="Следующий ход..."
-            value={input}
-            required
-            onChange={onInputChange}
-          />
-          <button type="submit">Ход</button>
-        </form>
-        <div className={s.answer}>answer to moove</div>
+        <input
+          type="text"
+          placeholder="Следующий ход..."
+          value={input}
+          required
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button onClick={onFormSubmit}>Ход</button>
+
+        <div className={s.answer}>{mooveAnswer}</div>
       </div>
-      <div className={s.result}>list of played words</div>
+      <div className={s.result}>{wordsArray}</div>
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import s from './Header.module.css';
+import React from "react";
+import s from "./Header.module.css";
 
-let article ='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+let article =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
 const Header = () => {
-    return <header className={s.wraper}>
-        <div className={s.header}>Uroboros</div>
-        <div>Правила игры</div>
-        <div className={s.article}>{article}</div>
-    </header >
-} 
+  return (
+    <header className={s.wraper}>
+      <div className={s.header}>Uroboros</div>
+      <div>Правила игры</div>
+      <div className={s.article}>{article}</div>
+    </header>
+  );
+};
 
 export default Header;
